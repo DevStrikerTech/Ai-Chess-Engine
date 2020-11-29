ecc0e22dcf6f48569b01c789310bf10ab0c2dc3b
King Cobra <kcobra557@gmail.com>
2020-11-29 12:23:26 +0000

    :chess_pawn: defined variables with imports

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import pygame
+from chess.variable_declaration import board_width, board_height
 
-window = pygame.display.set_mode((width, height))
+window = pygame.display.set_mode((board_width, board_height))
+pygame.display.set_caption('Chess Engine')
