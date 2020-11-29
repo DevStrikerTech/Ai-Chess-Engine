b9f0ddbcb88293340e49379be9cfd7a1eb856d23
King Cobra <kcobra557@gmail.com>
2020-11-29 12:29:02 +0000

    :chess_pawn: set engine main method and FPS

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import pygame
 from chess.variable_declaration import board_width, board_height
 
+FPS: int = 60
 window = pygame.display.set_mode((board_width, board_height))
 pygame.display.set_caption('Chess Engine')
+
+
+def main():
+    start_chess_engine = True
+    runtime = pygame.time.Clock()
+
+    while start_chess_engine:
+        runtime.tick(FPS)
+        pass
+
+
+main()
