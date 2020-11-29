237cc3f36eab20324f1ebb2af4b51d8ba27d8d2c
King Cobra <kcobra557@gmail.com>
2020-11-29 16:33:01 +0000

    :chess_pawn: added user interaction with chess pieces

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from chess.variable_declaration import board_width, board_height
+from chess.variable_declaration import board_width, board_height, board_square_size
 from chess.board import Board
 
 # Initialising display
@@ -8,14 +8,19 @@ window = pygame.display.set_mode((board_width, board_height))
 pygame.display.set_caption('Chess Engine')
 
 
+# User interaction with mouse
+def get_board_row_col_from_mouse(position):
+    x, y = position
+    board_row = y // board_square_size
+    board_column = x // board_square_size
+    return board_row, board_column
+
+
 # Event loop
 def main():
     start_chess_engine = True
     runtime = pygame.time.Clock()
     chess_board = Board()
-
-    piece = chess_board.get_pieces(0, 1)
-    chess_board.move_pieces(piece, 4, 3)
 
     while start_chess_engine:
         runtime.tick(FPS)
@@ -25,7 +30,10 @@ def main():
                 start_chess_engine = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                position = pygame.mouse.get_pos()
+                board_row, board_column = get_board_row_col_from_mouse(position)
+                piece = chess_board.get_pieces(board_row, board_column)
+                chess_board.move_pieces(piece, 4, 3)
 
         chess_board.draw_pieces(window)
         pygame.display.update()
